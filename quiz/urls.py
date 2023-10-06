d167cf8418c75de9242e01d5c9c53a103915118f
Smikey <smikeywork@gmail.com>
2023-10-07 02:00:48 +1100

    :hammer:Done CRUD on category API

--- quiz/urls.py
+++ quiz/urls.py
@@ -4,11 +4,14 @@ from rest_framework.routers import DefaultRouter
 
 # creating default router
 router = DefaultRouter()
+router.register("id",CategoryViewSet,basename="category")
 
 # creating endpoints
 urlpatterns = [
     path('', index, name="index"),
-    path('category', CategoryView.as_view())
+    path('category', CategoryView.as_view()),
+    path('category/<str:category_id>', CategoryView.as_view()),
+    path('category/', include(router.urls)),
 ]
 
 # urlpatterns += router.urls
--- quiz/views.py
+++ quiz/views.py
@@ -1,8 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view,action
 from .helpers import success,failure
-from rest_framework import status
+from rest_framework import status,viewsets
 from .models import *
 from .serializers import *
 
@@ -10,7 +10,6 @@ from .serializers import *
 @api_view(['get'])
 def index(request):
     return Response(success(message="Welcome to Quiz API",status=status.HTTP_200_OK))
-
 
 class CategoryView(APIView):
     def get(self,request):
@@ -21,3 +20,55 @@ class CategoryView(APIView):
         except Exception as ex:
             return Response(failure("Something went wrong",ex))
         
+    def post(self,request):
+        try:
+            data = request.data
+            serializer = CategorySerializer(data=data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(success("Category Created Successfully",serializer.data,status.HTTP_201_CREATED))
+            else:
+                return Response(failure(serializer.errors,status.HTTP_400_BAD_REQUEST))
+        except Exception as ex:
+            return Response(failure(f"Exception: {ex}",status.HTTP_417_EXPECTATION_FAILED))
+
+    def patch(self,request,category_id=None):
+        try:
+            category_obj = Category.objects.get(_id=category_id)
+            category_name = request.data
+            serializer = CategorySerializer(category_obj,data=category_name,partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(success("Category Updated Successfully",serializer.data,status.HTTP_200_OK))
+            else:
+                return Response(failure(serializer.errors,status.HTTP_400_BAD_REQUEST))
+        except Category.DoesNotExist:
+            return Response(failure("Category not found", status.HTTP_404_NOT_FOUND))
+        except Exception as ex:
+            return Response(failure(f"Exception: {ex}",status.HTTP_417_EXPECTATION_FAILED))
+
+    def delete(self,request,category_id):
+        try:
+            category_obj = Category.objects.get(_id=category_id)
+            category_name = category_obj.category_name  # Get the name of the category for the response message
+            category_obj.delete()  # Delete the category
+            return Response(success(f"Category '{category_name}' Deleted Successfully", data={}, status=status.HTTP_204_NO_CONTENT))
+        except Category.DoesNotExist:
+            return Response(failure("Category not found", status.HTTP_404_NOT_FOUND))
+        except Exception as ex:
+            return Response(failure(f"Exception: {ex}",status.HTTP_417_EXPECTATION_FAILED))
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+    @action(detail=True, methods=['GET'])
+    def get_single_data(self, request, pk=None):
+        try:
+            category_obj = self.get_object()
+            serializer = CategorySerializer(category_obj)
+            return Response(success("Single Category Fetched Successfully", serializer.data, status.HTTP_200_OK))
+        except Category.DoesNotExist:
+            return Response(failure("Category not found", status.HTTP_404_NOT_FOUND))
+        except Exception as ex:
+            return Response(failure(f"Exception: {ex}", status.HTTP_500_INTERNAL_SERVER_ERROR))
