40247d85d06296b37e59c7810b0ce823694ebe2d
Smikey <smikeywork@gmail.com>
2023-10-13 01:39:11 +1100

    :ambulance: Adding CROS

--- core/settings.py
+++ core/settings.py
@@ -73,13 +73,13 @@ MIDDLEWARE = [
 
 
 # Cors Allowed Origins
-#TODO 4: Replace your CORS_ALLOWED_ORIGINS here
-# CORS_ALLOWED_ORIGINS = os.environ.get("CORS_ALLOWED_ORIGINS").split(" ")
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:8000",
     "https://drf-quiz-react.onrender.com"
 ]
+#TODO 4: Replace your CORS_ALLOWED_ORIGINS here
+CORS_ALLOWED_ORIGINS = os.environ.get("CORS_ALLOWED_ORIGINS").split(" ")
 
 # Allowing UI Tweaks to the admin panel
 JAZZMIN_SETTINGS["show_ui_builder"] = True
